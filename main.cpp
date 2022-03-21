68df0b5b2f1f46527e61b485dd0b039356d2e59c
Nathan Withers <nathan.withers@sky.com>
2022-03-21 11:39:53 +0000

    Added boilerplate code for a window
    Created a boilerplate window using this tutorial https://www.youtube.com/watch?v=45MIykWJ-C4&t=1s, with code from https://github.com/VictorGordan/opengl-tutorials/tree/main/YoutubeOpenGL%201%20-%20Create%20Window

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+// Constant variables to set the size of the window
+const unsigned int width = 800;
+const unsigned int height = 800;
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+
+
+
+	// Set what version of OpenGL is being used (3.3)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Only need core profile functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+
+	// Create a GLFWwindow object of 800 by 800 pixels
+	GLFWwindow* window = glfwCreateWindow(width, height, "COMP3931 Project", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+
+
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	glViewport(0, 0, width, height);
+
+
+
+	// Specify the color of the background
+	glClearColor(0.203f, 0.450f, 0.615f, 1.0f);
+	// Clean the back buffer and assign the new color to it
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap the back buffer with the front buffer
+	glfwSwapBuffers(window);
+
+
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+
+
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+	return 0;
+}
