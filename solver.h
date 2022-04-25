09c49d3089cb658b68e072747b4d4b990437314e
Nathan Withers <nathan.withers@sky.com>
2022-04-25 13:39:40 +0100

    Tested a 3d implementation
    Experimented with a 3d implementation of the solver

--- solver.h
+++ solver.h
@@ -7,4 +7,5 @@ void add_source(int N, float* x, float* s, float dt);
 void project(int N, float* u, float* v, float* p, float* div);
 void dens_step(int N, float* x, float* x0, float* u, float* v, float diff, float dt);
 void vel_step(int N, float* u, float* v, float* u0, float* v0, float visc, float dt);
+void lin_solve(int N, int b, float* x, float* x0, float a, float c);
 
